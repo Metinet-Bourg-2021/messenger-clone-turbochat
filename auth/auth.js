e01f1d2ac3b6d6c1953d43929173edbacb708071
p1908624 <baptiste.rieber@etu.univ-lyon1.fr>
2021-11-15 15:22:27 +0100

    Correction gestion du token

--- auth/auth.js
+++ auth/auth.js
@@ -1,12 +1,19 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 function checkAuth(token, callback) {
   if (!token) {
     return callback({ code: "NOT_AUTHENTICATED", data: {} });
   }
-  const decodedToken = jwt.verify(token, "secret_key");
-  const userId = decodedToken.userId;
-  if (token.userId && token.userId !== userId) {
+  try {
+    const decodedToken = jwt.verify(token, "secret_key");
+    const user = User.findById(decodedToken.userId);
+    if (!user) {
+      throw new error();
+    } else {
+      return user._id;
+    }
+  } catch {
     return callback({ code: "NOT_AUTHENTICATED", data: {} });
   }
 }
--- controllers/conversationController.js
+++ controllers/conversationController.js
@@ -1,11 +1,17 @@
 const Conversation = require("../models/Conversation");
+const { checkAuth } = require("../auth/auth");
+const User = require("../models/User");
 
-
-
-
-
-
+async function getConversations({ token }, callback) {
+  checkAuth(token, callback);
+  const user = await User.findById(token.userId);
+  if (!user) {
+    callback({ code: "NOT_AUTHENTICATED", data: {} });
+  } else {
+    const conversations = Conversation.find().where();
+  }
+}
 
 module.exports = {
-    
-}
+  getConversations: getConversations,
+};
